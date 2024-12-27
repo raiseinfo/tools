600bb4d04a4a3e6c71d988595dc0199460e4fb50
David Bonaparte <494901823@qq.com>
2024-12-27 08:17:54 +0800

    更新依赖

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'raiseinfo/tools',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '64a1bd39be3a976fa3df37363a8b3dbe283ff8c7',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'raiseinfo/tools' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '64a1bd39be3a976fa3df37363a8b3dbe283ff8c7',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
